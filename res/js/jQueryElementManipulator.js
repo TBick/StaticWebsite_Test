869b61dbeaf242428e6c02cf7a4b1cbc6da07339
TBick <troy.bick00@gmail.com>
2016-12-30 11:35:58 -0600

    Finished jQuery Element Manipulation

--- res/js/jQueryElementManipulator.js
+++ res/js/jQueryElementManipulator.js
@@ -1,1 +1,25 @@
 
+var clickCount = 0;
+var flag = true;
+
+function setEventListeners() {
+
+	$('#clicks').click(function() {
+		clickCount++;
+		$(this).text(String(clickCount));
+	});
+
+	$('#status').click(function() {
+		flag = !flag;
+		$(this).text(String(flag).toUpperCase());
+	});
+
+}
+
+
+$(document).ready(function() {
+	setEventListeners();
+
+	$('#clicks').text(String(clickCount));
+	$('#status').text(String(flag).toUpperCase());
+});
